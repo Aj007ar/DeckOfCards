8ab7b8862bc9fefb1f5dd15b49575ccc9a90e1c9
Aj007ar <aj007.ar@gmail.com>
2022-08-26 14:25:09 +0530

    [Ajay] Added Deck Of Card programs

---
+++ DeckOfCards/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace DeckOfCards
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome Deck Of Card");
+            PlayingCards playingCards = new PlayingCards();
+            playingCards.ShuffleCards();
+        }
+    }
+}
